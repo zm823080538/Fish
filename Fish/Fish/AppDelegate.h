b17d3495c44e341c87e13e42226e97c3ab9f3d18
zhaoming <mingzhaoa@isoftstone.com>
2018-04-07 09:02:49 +0800

    Init project

---
+++ Fish/Fish/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Fish
+//
+//  Created by zhaoming on 07/04/2018.
+//  Copyright © 2018 zhaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Fish/Fish/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Fish
+//
+//  Created by zhaoming on 07/04/2018.
+//  Copyright © 2018 zhaoming. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
