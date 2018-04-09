9d7c01de7bd2b300afcd6a81b01cf2c79667d4c8
zhaoming <mingzhaoa@isoftstone.com>
2018-04-09 17:59:12 +0800

    init project

--- Fish/Fish/ViewController/Login/LoginViewController.h
+++ Fish/Fish/ViewController/Login/LoginViewController.h
@@ -1,15 +1,13 @@
 //
-//  ViewController.h
+//  LoginViewController.h
 //  Fish
 //
 //  Created by zhaoming on 07/04/2018.
 //  Copyright © 2018 zhaoming. All rights reserved.
 //
 
-#import <UIKit/UIKit.h>
+#import "LLBaseViewController.h"
 
-@interface ViewController : UIViewController
-
+@interface LoginViewController : LLBaseViewController
 
 @end
-
